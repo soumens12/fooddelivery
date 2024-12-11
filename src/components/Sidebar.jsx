ff8c3705661d8ac8c9b67d3670685285a74749f8
soumens12 <101258865+soumens12@users.noreply.github.com>
2024-12-11 19:50:10 +0530

    sidebar updated

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,28 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from '../img/logo.svg';
 
 function Sidebar({isOpen, toggleSidebar, menuItems}) {
+
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
   return (
-    <div className={`offcanvas offcanvas-start ${isOpen ? "show" : ""}`} style={{ visibility: isOpen ? "visible" : "hidden" }}>
+    <div className={`offcanvas offcanvas-start ${isOpen ? "show" : ""}`} style={{ visibility: isOpen ? "visible" : "hidden" }} id="offcanvas" aria-labelledby="offcanvasLabel">
       <div className="offcanvas-header">
         <h5 className="offcanvas-title">
           <img src={logo} alt="" />
         </h5>
-        <button type="button" className="btn-close" onClick={toggleSidebar}></button>
+        <button type="button" className="btn-close" onClick={toggleSidebar} ></button>
       </div>
       <div className="offcanvas-body">
-        <ul className="list-unstyled">
+        <ul className="list-unstyled sidebar">
           {menuItems.map((item, index) =>
             item.subItems ? (
-              <li key={index}>
+              <li key={index} onClick={toggleDropdown}>
                 {item.title}
-                <ul className="ps-3">
+                <span>
+                {isDropdownOpen ? (
+                  <i className="bx bx-chevron-up"></i>
+                ) : (
+                  <i className="bx bx-chevron-down"></i>
+                )}
+                </span>
+
+                {isDropdownOpen && (
+                <ul className="submenu">
                   {item.subItems.map((subItem, subIndex) => (
                     <li key={subIndex}>
                       <a href="/">{subItem}</a>
                     </li>
                   ))}
                 </ul>
+                )}
               </li>
             ) : (
               <li key={index}>
